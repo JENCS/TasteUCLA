6ce5f57115aae94e0f40945ffc2badaa3ebb4f80
Eunsoo An <eunsooan0@gmail.com>
2023-11-07 12:53:49 -0800

    Implement single page application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateReview from './pages/CreateReview';
+import ShowReview from './pages/ShowReview';
+import EditReview from './pages/EditReview';
+import DeleteReview from './pages/DeleteReview';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>TasteUCLA</div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/reviews/create' element={<CreateReview />} />
+      <Route path='/reviews/detail/:id' element={<ShowReview />} />
+      <Route path='/reviews/edit/:id' element={<EditReview />} />
+      <Route path='/reviews/delete/:id' element={<DeleteReview />} />
+    </Routes>
   );
 }
 
