f1c6689ed258f6b6de49afeb3e7932cb27bb23ba
Jarod Houston <jarodhouston26@gmail.com>
2023-11-28 14:42:31 -0800

    Change SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,13 +1,24 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import resets from "../styles/_resets.module.css";
+import classes from "../styles/LoginPage.module.css";
+import sign_classes from "../styles/SignUpPage.module.css";
+import Button from "@mui/material/Button";
 
-export default function SignUpPage({ changeLoginState }) {
+function SignUpPage() {
   return (
-    <div>
-      <p>p</p>
-      <p>p</p>
-      <Link to="/">
-        <button onClick={changeLoginState}>Sign Up</button>
-      </Link>
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.tasteUCLA}>TasteUCLA</div>
+      <div className={classes.logo}></div>
+      <div className={classes.signUp}>Sign in</div>
+      <div className={classes.signUpButton}>
+        <Button
+          name="signup_button"
+          style={{ width: "145.2855px" }}
+          href="/login"
+        ></Button>
+      </div>
     </div>
   );
 }
+
+export default SignUpPage;
