9d1486ddb482d38b27c600e7ddac93bc2e12f41a
cainmo <cainomomo@gmail.com>
2023-11-30 19:16:46 -0800

    Add search functionality skeletons

---
+++ backend/controllers/searchController.js
@@ -0,0 +1,30 @@
+import { Restaraunt } from "../models/Restaurant.js"
+import { User } from "../models/User.js"
+import { Food } from "../models/Food.js"
+import { Review } from "../models/Review.js"
+import asyncHandler from "express-async-handler"
+
+const SEARCH_RESULT_CAP = 100;
+
+// @desc Get search hits
+// @route GET /database
+// @access Private
+const searchDatabase = asyncHandler(async (req, res) => {
+
+    var searchResults = [];
+    const rests = await Restaurant.find().lean();
+    const users = await User.find().select("-password").lean();
+    const foods = await Food.find().lean();
+    const reviews = await Review.find().lean();
+
+    for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
+        
+    }
+
+    if (!searchResults?.length) {
+        return res.status(400).json({ message: "No users found" });
+    }
+    res.json(searchResults)
+})
+
+export { searchDatabase }
