cf087575f26f337f41db27099796ecf197dac96a
cainmo <cainomomo@gmail.com>
2023-12-03 02:47:37 -0800

    Add meat to search functionality

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,4 +1,4 @@
-import { Restaraunt } from "../models/Restaurant.js"
+import { Restaurant } from "../models/Restaurant.js"
 import { User } from "../models/User.js"
 import { Food } from "../models/Food.js"
 import { Review } from "../models/Review.js"
@@ -7,26 +7,121 @@ import asyncHandler from "express-async-handler"
 const SEARCH_RESULT_CAP = 100;
 
 // @desc Get search hits
-// @route GET /database
+// @route GET /search
 // @access Private
 const searchDatabase = asyncHandler(async (req, res) => {
 
-    var searchResults = [1, 2, 3 , "123123"];
-    const rests = await Restaurant.find().lean();
-    const users = await User.find().select("-password").lean();
-    const foods = await Food.find().lean();
-    const reviews = await Review.find().lean();
+    if (!searchResults?.length) {
+        console.log("fail")
+        return res.status(400).json({ message: "No data found" });
+    }
+    res.send(searchResults)
+})
 
+// @desc Get search hits
+// @route GET /search
+// @access Private
+const searchRestaurants = asyncHandler(async (req, res) => {
     
+    if (! ((typeof req.body == "object") || (typeof req.body?.query == "string"))) {
+        return res.status(400).json({ message: "Invalid query" });
+    }
+    
+    let query = req.body.query;
+    var searchResults = [];
 
-    for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
-        
+    const rests = await Restaurant.find( {} ).lean();
+
+    for (let i in reviews) {
+        let loc = reviews[i].title.search(query);
+        if (!(-1 === loc)) {
+            console.log("hit on review: " + reviews[i].title + " at loc: " + loc);
+        }
     }
 
+    // for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
+    //     if (reviews[i].)
+    // }
+
     if (!searchResults?.length) {
+        console.log("fail")
         return res.status(400).json({ message: "No users found" });
     }
     res.send(searchResults)
 })
 
-export { searchDatabase }
+// @desc Get search hits
+// @route GET /search
+// @access Private
+const searchUsers = asyncHandler(async (req, res) => {
+    
+    if (! ((typeof req.body == "object") || (typeof req.body?.query == "string"))) {
+        return res.status(400).json({ message: "Invalid query" });
+    }
+    
+    let query = req.body.query;
+    var searchResults = [];
+
+    // const rests = await Restaurant.find({ n }).lean();
+    // const users = await User.find().select("-password -profile_picture").lean();
+    // const foods = await Food.find().lean();
+    const reviews = await Review.find().lean();
+
+    // console.log(reviews);
+
+    for (let i in reviews) {
+        let loc = reviews[i].title.search(query);
+        if (!(-1 === loc)) {
+            console.log("hit on review: " + reviews[i].title + " at loc: " + loc);
+        }
+    }
+
+    // for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
+    //     if (reviews[i].)
+    // }
+
+    if (!searchResults?.length) {
+        console.log("fail")
+        return res.status(400).json({ message: "No users found" });
+    }
+    res.send(searchResults)
+})
+
+// @desc Get search hits
+// @route GET /search
+// @access Private
+const searchReviews = asyncHandler(async (req, res) => {
+    
+    if (! ((typeof req.body == "object") || (typeof req.body?.query == "string"))) {
+        return res.status(400).json({ message: "Invalid query" });
+    }
+    
+    let query = req.body.query;
+    var searchResults = [];
+
+    // const rests = await Restaurant.find({ n }).lean();
+    // const users = await User.find().select("-password -profile_picture").lean();
+    // const foods = await Food.find().lean();
+    const reviews = await Review.find().lean();
+
+    // console.log(reviews);
+
+    for (let i in reviews) {
+        let loc = reviews[i].title.search(query);
+        if (!(-1 === loc)) {
+            console.log("hit on review: " + reviews[i].title + " at loc: " + loc);
+        }
+    }
+
+    // for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
+    //     if (reviews[i].)
+    // }
+
+    if (!searchResults?.length) {
+        console.log("fail")
+        return res.status(400).json({ message: "No users found" });
+    }
+    res.send(searchResults)
+})
+
+export { searchDatabase, searchRestaurants, searchUsers, searchReviews };
--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -6,4 +6,12 @@ const router = express.Router()
 router.route('/')
     .get(searchController.searchDatabase)
 
+router.route('/restaurants')
+    .get(searchController.searchRestaurants)
+
+router.route('/users')
+    .get(searchController.searchUsers)
+
+router.route('/reviews')
+    .get(searchController.searchReviews)
 export default router
