a9c9f8db8b7ecdff79d6fe88c2d9340dabcb1e77
cainmo <cainomomo@gmail.com>
2023-12-02 19:28:22 -0800

    Add search controller and routes skeleton

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -11,11 +11,13 @@ const SEARCH_RESULT_CAP = 100;
 // @access Private
 const searchDatabase = asyncHandler(async (req, res) => {
 
-    var searchResults = [];
+    var searchResults = [1, 2, 3 , "123123"];
     const rests = await Restaurant.find().lean();
     const users = await User.find().select("-password").lean();
     const foods = await Food.find().lean();
     const reviews = await Review.find().lean();
+
+    
 
     for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
         
@@ -24,7 +26,7 @@ const searchDatabase = asyncHandler(async (req, res) => {
     if (!searchResults?.length) {
         return res.status(400).json({ message: "No users found" });
     }
-    res.json(searchResults)
+    res.send(searchResults)
 })
 
 export { searchDatabase }
