436121be8e69a350be757c77bb64e9928b1e64fb
cainmo <cainomomo@gmail.com>
2023-12-04 02:08:43 -0800

    Add query validation

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -10,90 +10,53 @@ const SEARCH_RESULT_CAP = 100;
 // @route GET /search
 // @access Private
 const searchGlobal = asyncHandler(async (req, res) => {
-    let query = req.body.query;
-    let searchResults;
-
-    if (!(typeof req.body?.query == "string")) {
+    
+    if (!isValidQuery(req)) {
         return res.status(400).json({ message: "Invalid query" });
     }
+
+    let query = req.body.query;
+    let searchResults;
 
     await searchDatabase(query, query, query).then( (result) => {
         searchResults = result;
     });
-    // let searchResults = ["haha"];
 
-    if (searchResults?.length < 1) {
-        return res.status(400).json({ message: "No matches found" });
-    }
-    res.send(searchResults);
+    res.send({ data: searchResults, hits: searchResults.length});
 })
 
 // @desc Get search hits on restaurants
 // @route GET /search/restaurants
 // @access Private
 const searchRestaurants = asyncHandler(async (req, res) => {
-    
-    
-
+    // TODO
 })
 
 // @desc Get search hits on users
 // @route GET /search/users
 // @access Private
 const searchUsers = asyncHandler(async (req, res) => {
-    
-    
+    // TODO
 })
 
 // @desc Get search hits on reviews
 // @route GET /search/reviews
 // @access Private
 const searchReviews = asyncHandler(async (req, res) => {
-    
-    if (! ((typeof req.body == "object") || (typeof req.body?.query == "string"))) {
-        return res.status(400).json({ message: "Invalid query" });
-    }
-    
-    let query = req.body.query;
-    var searchResults = [];
-
-    // const rests = await Restaurant.find({ n }).lean();
-    // const users = await User.find().select("-password -profile_picture").lean();
-    // const foods = await Food.find().lean();
-    const reviews = await Review.find().lean();
-
-    // console.log(reviews);
-
-    for (let i in reviews) {
-        let loc = reviews[i].title.search(query);
-        if (!(-1 === loc)) {
-            console.log("hit on review: " + reviews[i].title + " at loc: " + loc);
-        }
-    }
-
-    // for (let i = 0; i < SEARCH_RESULT_CAP; i++) {
-    //     if (reviews[i].)
-    // }
-
-    if (!searchResults?.length) {
-        console.log("fail")
-        return res.status(400).json({ message: "No users found" });
-    }
-    res.send(searchResults)
+    // TODO
 })
 
 // Helper Function (TODO)
 async function searchDatabase(restaurantQuery, userQuery, reviewQuery) {
 
-    var resultArray = [];
-    // const foods = await Food.find().lean();
+    let resultArray = [];
     
     if (restaurantQuery !== '') {
         const rests = await Restaurant.find({}).select("-image -reviews").lean();
-        console.log(rests);
+        const re = new RegExp(restaurantQuery , "gi");
 
         for (let i in rests) {
-            let found = rests[i].name.match(restaurantQuery);
+            let found = rests[i].name.match(re);
             if (!(found === null)) {
                 resultArray.push(rests[i]);
             }
@@ -112,6 +75,10 @@ async function searchDatabase(restaurantQuery, userQuery, reviewQuery) {
 
     return resultArray;
 
+}
+
+function isValidQuery(queryReq) {
+    return !((typeof queryReq?.body?.query !== "string") || (queryReq.body.query.match("[^a-zA-z0-9]") !== null));
 }
 
 export { searchGlobal, searchRestaurants, searchUsers, searchReviews };
