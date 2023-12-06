effca7900a279f4854685cb054a006f59ac8ab7b
Jarod Houston <jarodhouston26@gmail.com>
2023-12-05 16:02:00 -0800

    Add body-parser

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,47 @@
-import 'dotenv/config'
-import express from 'express'
-import mongoose from 'mongoose'
-import authRoutes from './routes/authRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import reviewRoutes from './routes/reviewRoutes.js'
-import searchRoutes from './routes/searchRoutes.js'
-import cors from 'cors'
-import corsOptions from './config/corsOptions.js'
-import cookieParser from 'cookie-parser'
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import searchRoutes from "./routes/searchRoutes.js";
+import cors from "cors";
+import corsOptions from "./config/corsOptions.js";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+//const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use(express.json())
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use(cors(corsOptions))
+app.use(express.json());
 
-app.use(cookieParser())
+app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    console.log(req)
-    return res.status(222).send('TasteUCLA')
-})
-app.use('/auth', authRoutes)
-app.use('/users', userRoutes)
-app.use('/reviews', reviewRoutes)
-app.use('/search', searchRoutes)
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(222).send("TasteUCLA");
+});
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/reviews", reviewRoutes);
+app.use("/search", searchRoutes);
 
 mongoose
-    .connect(process.env.DATABASE_URI)
-    .then(() => {
-        console.log('Server connected to database')
-        app.listen(PORT, () => {
-            console.log(`Server listening on port ${PORT}`)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+  .connect(process.env.DATABASE_URI)
+  .then(() => {
+    console.log("Server connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
