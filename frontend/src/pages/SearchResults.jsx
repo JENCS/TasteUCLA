6f60e9f277373d5db053a10193708927f82e1f2f
cainmo <cainomomo@gmail.com>
2023-12-05 20:21:35 -0800

    Flesh out search results page css, format, and data

--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -19,6 +19,7 @@ function SearchResults() {
                 setUserResults(res.data.users);
                 setReviewResults(res.data.reviews);
                 setLoading(false);
+                console.log(JSON.stringify(res.data.restaurants))
             })
             .catch((error) => {
                 console.error('Error fetching search results', error);
@@ -35,17 +36,36 @@ function SearchResults() {
                 {loading ? (
                 <Spinner />
                 ) : (
-                    <><div className={classes.reviews_grid}>
-                    {reviews.length === 0 ? (
-                        <div>No reviews matching "{query}" found</div>
-                        ) : (
-                        <div className={classes.reviews_grid}>
-                            {reviews.map((review, index) => (
-                                <div key={index}>review{index}</div>
-                            ))}
+                    <>
+                        <div className={classes.restaurants_container}>
+                            <div className={classes.searchResults_section_title}>Restaurants</div>
+                            {restaurants.length === 0 ? (
+                                <div>No restaurants matching "{query}" found</div>
+                            ) : (
+                                <div className={classes.restaurants_grid}>
+                                    {restaurants.map((rest, index) => (
+                                        <a href={"/locations/" + rest._id}>
+                                            <div key={index} className={classes.restaurants_grid_item}>
+                                                <div className={classes.restaurant_title}>{rest.name}</div>
+                                                <div>{rest.location}</div>
+                                            </div>
+                                        </a>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                        )}
-                    </div></>
+                        {/* <div className={classes.reviews_grid}>
+                        {reviews.length === 0 ? (
+                            <div>No reviews matching "{query}" found</div>
+                            ) : (
+                            <div className={classes.reviews_grid}>
+                                {reviews.map((review, index) => (
+                                    <div key={index}>{review.title}</div>
+                                ))}
+                            </div>
+                            )}
+                        </div> */}
+                    </>
                 )}
             </div>
         </div>
