b732c30363c26a9a2a20b84c1674a5c80bd89e8d
Jarod Houston <jarodhouston26@gmail.com>
2023-12-06 12:03:46 -0800

    Fix bug

--- frontend/src/pages/ShowReview.jsx
+++ frontend/src/pages/ShowReview.jsx
@@ -26,7 +26,7 @@ const ShowReview = () => {
       .then((res) => {
         setReview(res.data);
         setComments(res.data.comments);
-        setLoading(false);    
+        setLoading(false);
         setComments(res.data.comments);
       })
       .catch((error) => {
@@ -52,45 +52,55 @@ const ShowReview = () => {
               </p>
               <p className="content">{review.description}</p>
             </div>
-            <div className="created_time">{"Created at: " + review.createdAt}</div>
-            <div className="updated_time">{"Updated at: " + review.updatedAt}</div>
+            <div className="created_time">
+              {"Created at: " + review.createdAt}
+            </div>
+            <div className="updated_time">
+              {"Updated at: " + review.updatedAt}
+            </div>
             <div className="reviews-header">Comments</div>
             <div className="comments-grid">
-                <div className="comment-textbox">
-                    <form onSubmit={handleSubmit}>
-                        <textarea
-                            className="w-full p-2 text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline"
-                            rows="4"
-                            placeholder="Write your comment..."
-                            value={response}
-                            onChange={(e) => setResponse(e.target.value)}
-                        />
-                    <div className="text-right">
-                        <button type="submit" className="px-4 py-2 mt-2 text-white bg-blue-300 rounded hover:bg-blue-700">Submit</button>
-                    </div>
-                    </form>
-                </div>
-                <div className="comments-grid">
-                {comments.map((comment, index) => (
-                   <div className="comments_grid_item" key={index}>
-                    <div className="comments_content">
-                        <div className="comments_user_container">
-                          <div className="comments_username">{"user"}</div>
-                          <span className="comments_profile_pic"></span>
-                        </div>
-                        <div className="comments_description">
-                        {comment.body}
-                        </div>
-                    </div>
-                    <div className="created_time_comment">{"Created at: " + comment.createdAt}</div>
-                    <div className="updated_time_comment">{"Updated at: " + comment.updatedAt}</div>
+              <div className="comment-textbox">
+                <form onSubmit={handleSubmit}>
+                  <textarea
+                    className="w-full p-2 text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline"
+                    rows="4"
+                    placeholder="Write your comment..."
+                    value={response}
+                    onChange={(e) => setResponse(e.target.value)}
+                  />
+                  <div className="text-right">
+                    <button
+                      type="submit"
+                      className="px-4 py-2 mt-2 text-white bg-blue-300 rounded hover:bg-blue-700"
+                    >
+                      Submit
+                    </button>
                   </div>
-                  ))} 
-                </div>
-                
+                </form>
+              </div>
+              <div className="comments-grid">
+                {comments.map((comment, index) => (
+                  <div className="comments_grid_item" key={index}>
+                    <div className="comments_content">
+                      <div className="comments_user_container">
+                        <div className="comments_username">{"user"}</div>
+                        <span className="comments_profile_pic"></span>
+                      </div>
+                      <div className="comments_description">{comment.body}</div>
+                    </div>
+                    <div className="created_time_comment">
+                      {"Created at: " + comment.createdAt}
+                    </div>
+                    <div className="updated_time_comment">
+                      {"Updated at: " + comment.updatedAt}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
-                         
-        </div>    
+          </div>
+        </div>
       )}
     </div>
   );
