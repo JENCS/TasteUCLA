25fbd0cd133abbd882b289952bd1ed1c9e3a9c38
cainmo <cainomomo@gmail.com>
2023-12-08 18:45:30 -0800

    Update allowed origins for CORS

--- backend/config/allowedOrigins.js
+++
@@ -1,5 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:5173'
-]
-
-export default allowedOrigins
--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,14 +1,8 @@
-import allowedOrigins from "./allowedOrigins.js"
-
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    // origin: (origin, callback) => {
-    //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-    //         callback(null, true)
-    //     } else {
-    //         callback(new Error('Not allowed by CORS'))
-    //     }
-    // },
+    origin: [
+        'http://localhost:4173',
+        'http://localhost:5173'
+    ],
     credentials: true,
     optionsSuccessStatus: 200
 }
