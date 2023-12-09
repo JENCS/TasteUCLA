e8e7eed0c2504573f4683b671e9e11bee7386886
Jarod Houston <jarodhouston26@gmail.com>
2023-12-08 18:50:43 -0800

    Fix minor bugs/issues throughout

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,32 +1,25 @@
-import { Link } from 'react-router-dom';
-import { BsArrowLeft } from 'react-icons/bs';
-import { useNavigate } from "react-router-dom"
+import "../styles/BackButton.css";
+import { BsArrowLeft } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 
 const BackButton = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    const handleGoBack = () => {
-        navigate(-1);
-    };
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
-    return (
-        <div className='flex'>
-            {/* <Link
-                to={destination}
-                className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
-            >
-                <BsArrowLeft className='text-2xl' />
-            </Link> */}
-            <Button
-                onClick={handleGoBack}
-                className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
-            >
-                <BsArrowLeft className='text-2xl' />
-            </Button> 
-        </div>
-    );
-}
+  return (
+    <div className="flex">
+      <Button
+        onClick={handleGoBack}
+        className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
+      >
+        <BsArrowLeft className="text-2xl" />
+      </Button>
+    </div>
+  );
+};
 
 export default BackButton;
